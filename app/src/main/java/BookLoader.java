8ca177bd49019a0df2d46828a90539f7316eb979
ZAINAB <abdulaizainab@gmail.com>
2018-10-13 23:26:24 +0100

    Initial commit

---
+++ app/src/main/java/BookLoader.java
@@ -0,0 +1,25 @@
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+public class BookLoader extends AsyncTaskLoader{
+
+        // Variable that stores the search string.
+        private String mQueryString;
+
+        // Constructor providing a reference to the search term.
+        public BookLoader(Context context, String queryString) {
+            super(context);
+            mQueryString = queryString;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad(); // Starts the loadInBackground method
+        }
+
+        @Override
+        public String loadInBackground() {
+            return NetworkUtils.getBookInfo(mQueryString);
+        }
+    }
+
+
